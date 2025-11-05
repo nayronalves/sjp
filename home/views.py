7c40b99c6268ecbfdcd96f04086ce38996f525d3
Nayron Alves <nayronalves.info@gmail.com>
2025-11-05 16:46:52 -0300

    Melhoria no layout principal/home

--- home/views.py
+++ home/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
-from home.models import Associate
-
-
 # Create your views here.
 def home(request):
-    associates = Associate.objects.all()
-    return render(request, "base.html", {"associates": associates})
+    return render(request, "home/index.html")
