bb48a26672d1b3be4f024c1fbe6a64d086782ed0
Nayron Alves <nayronalves.info@gmail.com>
2025-11-05 10:59:02 -0300

    Ajuste do settings

--- serjuspi/settings.py
+++ serjuspi/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATICFILES_DIRS = BASE_DIR / 'static'
 else:
     STATIC_ROOT = BASE_DIR / 'static'
 
