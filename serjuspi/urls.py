60e77c3a45b580387a5cce1dc5236b1aa628fe1b
Nayron Alves <nayronalves.info@gmail.com>
2025-11-05 10:32:28 -0300

    Remoção static config no home/urls.py

--- serjuspi/urls.py
+++ serjuspi/urls.py
@@ -23,4 +23,4 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
